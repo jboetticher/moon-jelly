718045ef3a4a518097d26a990f20c0e1da53d8f7
pisslake <pisslake@users.noreply.github.com>
2020-12-10 20:34:35 -0800

    set up module index.js system

---
+++ moon-jelly-ext/src/modules/google_module/index.js
@@ -0,0 +1,14 @@
+// google integration
+
+import React from 'react';
+
+const google_module = () => (
+    <div>Google Drive Module</div>
+);
+
+export default {
+    props: {
+        hasPanel: "true"
+    },
+    name: 'google_module',
+}
