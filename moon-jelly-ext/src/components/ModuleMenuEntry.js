fd68ac816a2345c9bf3009972c5bc92e47b87130
pisslake <pisslake@users.noreply.github.com>
2020-12-12 23:39:15 -0800

    Stores moduel checkbox data in local web storage

--- moon-jelly-ext/src/components/ModuleMenuEntry.js
+++ moon-jelly-ext/src/components/ModuleMenuEntry.js
@@ -9,24 +9,77 @@ export default class ModuleMenuEntry extends Component {
         }
     }
 
+    // Check if we can use web storage in the browser
+    storageAvailable(type) {
+        var storage;
+        try {
+            storage = window[type];
+            var x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch (e) {
+            return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                // acknowledge QuotaExceededError only if there's something already stored
+                (storage && storage.length !== 0);
+        }
+    }
+
+
 
     // Flips the state of the checkbox whenever it is clicked
-    changeState() {
-        let prevState = this.state.enabled;
+    // Stores new value in localstorage
+    changeStoredState() {
+
+        // Change component state off of the stored state
+        let prevState = this.getStoredState();
         this.setState({
             enabled: !prevState
         });
+
+        // Store changed state in localstorage
+        if (this.storageAvailable('localStorage')) {
+            localStorage.setItem(this.props.name, !prevState);
+            //console.log("stored", localStorage.getItem(this.props.name));
+        }
+    }
+
+    // Grabs the state of the checkbox from localStorage
+    getStoredState() {
+        if (this.storageAvailable('localStorage')) {
+            //console.log("retrieved", localStorage.getItem(this.props.name));
+
+            // Stored as string, but we need a boolean value
+            let val = localStorage.getItem(this.props.name) == 'true' ? true : false;
+
+            return val;
+        }
+        else {
+            // if localstorage is unavailable, use component state
+            return this.state.enabled;
+        }
         
     }
+
 
     render() {
         return (
             <div>
-                <input type="checkbox" checked={this.state.enabled} onChange={()=>this.changeState()}/>  
+                <input type="checkbox" checked={this.getStoredState()} onChange={() => this.changeStoredState()} />
 
                 <Button
-                    primary={this.props.selected == this.props.name} noRound    
-                    onClick={this.props.setNextPanel.bind(this, this.props.name)}   
+                    primary={this.props.selected == this.props.name} noRound
+                    onClick={this.props.setNextPanel.bind(this, this.props.name)}
                 >
                     {this.props.name}
                 </Button>
