cc1d22cf416c61de8579244ef42c5d9069b30265
pisslake <pisslake@users.noreply.github.com>
2020-12-25 15:18:42 -0800

    the essentials of the alert system seems to work (not throughly tested)

--- moon-jelly-ext/public/alertsPage.js
+++ moon-jelly-ext/public/alertsPage.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     alert("sick im installed");
     console.log("bro");
     startAlarm();
@@ -7,26 +7,27 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.runtime.onStartup.addListener(() => {
     console.log('onStartup....');
     startAlarm();
-  });
+});
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
+chrome.alarms.onAlarm.addListener(function (alarm) {
     console.log("alram");
 
-    console.log(window.localStorage.getItem("keywordDate_rinkeby"));    
-    console.log("this one is: ", window.localStorage.getItem("lol no exist")); 
+    //console.log(window.localStorage.getItem("keywordDate_rinkeby"));    
+    //console.log("this one is: ", window.localStorage.getItem("lol no exist")); 
 
     if (alarm.name === "refetch") {
-      //alert("yo 1 min");
-      console.log("1 min pass yay");
-      fetchOceanData("dab").then(res => console.log(res));
+        //alert("yo 1 min");
+        console.log("1 min pass yay");
+        //fetchOceanData("test", 1).then(res => console.log(res));
+        filterNewAssets();
     }
 });
 
-function startAlarm(){
+function startAlarm() {
     chrome.alarms.get("refetch", alarm => {
-        if(alarm){
+        if (alarm) {
             // alarm exists, do nothing
-        } else{
+        } else {
             // start the alarm since it doesn't exist
             chrome.alarms.create("refetch", {
                 delayInMinutes: 1,
@@ -36,35 +37,92 @@ function startAlarm(){
     })
 }
 
-function filterNewAssets(){
+function filterNewAssets() {
+    // get the filter date created by the the alerts panel
     let filterDate = window.localStorage.getItem("keywordDate_rinkeby");
-    filterDate != null ? filterDate : new Date().toISOString();
+
+    // if no date, nothing to process
+    if (filterDate == null) return;
+
+    // get the keywords created by alerts panel (should be an array after parsing)
+    let keywords = JSON.parse(window.localStorage.getItem("keywords_rinkeby"));
+
+    // if no keywords, nothing to process
+    if (keywords == null) return;
+
+    let promiseArray = [];
+    keywords.forEach((keyword, index) => {
+        promiseArray.push(fetchRecentOceanData(keyword, 1));
+    });
+
+    // array to keep track of new assets (assets that were created past the filter date)
+    // use a set to prevent duplicates
+    let filteredAssets = new Set();
+
+    // Evaluate all fetched promises
+    Promise.all(promiseArray).then((data) => {
+        //console.log(data);
+
+        // Loop through the array of keyword results
+        // each entry is the fetched results of a diff keyword
+        data.forEach((keywordResult) => {
+            keywordResult['results'].forEach((asset) => {
+
+                // Compare the creation date with the filter date
+                // If the creation date is greater, it is a new asset
+                if(asset['created'] >= filterDate){
+                    console.log("NEW ASSET FOUND", asset);
+
+                    // add the asset DID into the set
+                    filteredAssets.add(asset['id']);
+                }
+            })
+        });
+
+        // grab the old array
+        let keywordAssets = window.localStorage.getItem("keywordAssets_rinkeby");
+
+        // merge the old array with the newly found new assets
+        let merged = keywordAssets.concat(Array.from(filteredAssets));
+
+        // Store the merged assets in local storage (Stringify an array) 
+        window.localStorage.setItem("keywordAssets_rinkeby", JSON.stringify(merged));
+
+        // Update date to current date (don't want to get new assets again)
+        window.localStorage.setItem("keywordDate_rinkeby", new Date().toISOString());
+    });
+
+    //fetchRecentOceanData("test", 1).then(res => console.log(res));    
+
 }
 
-async function fetchOceanData(keyword){
+async function fetchRecentOceanData(keyword, page) {
     let network = 'rinkeby';
 
-    return fetch('https://aquarius.' + network + '.oceanprotocol.com/api/v1/aquarius/assets/ddo/query', {
+    // post method stopped working for some reason
+    /*return fetch('https://aquarius.' + network + '.oceanprotocol.com/api/v1/aquarius/assets/ddo/query', {
         method: 'POST',
         mode: 'cors',
         headers: {
-          "Content-Type": 'application/json'
+            "Content-Type": 'application/json'
         },
+        cache: 'no-cache', 
+        credentials: 'same-origin', 
         body: JSON.stringify({
             "page": 1,
             "offset": 100,
             "query": {
-                "nativeSearch": 1,
+                "native ": 1,
                 "query_string": {
                     "query": "-isInPurgatory:true"
                 }
             },
-            "sort": { 
-                "created": 1,
-                "dataToken": 1 
-            },
+            "sort": {"created": -1},
             "text": keyword
         })
     })
-    .then(res => res.json());
+    .then(res => res.json());*/
+
+    return fetch('https://aquarius.' + network + '.oceanprotocol.com/api/v1/aquarius/assets/ddo/query?text=' + keyword + '&page=' + page + '&sort={"created": -1}')
+            .then(data => data.json());
 }
--- moon-jelly-ext/src/components/complete/Alerts.js
+++ moon-jelly-ext/src/components/complete/Alerts.js
@@ -15,21 +15,51 @@ import { useOcean } from '@oceanprotocol/react';
 import ReactTagInput from "@pathofdev/react-tag-input";
 import "@pathofdev/react-tag-input/build/index.css";
 
-let Alerts = props => {    
+let Alerts = props => {
 
     const network = useOcean()['config']['network'];
     const { storeToLocal, storeArrayToLocal, getArrayFromLocal } = useWebStorage();
-    const { fetchDataBySort } = useAquariusFetch();
+    const { fetchDDO } = useAquariusFetch();
 
     // retrieve keywords from localStorage
     // example keyword is only displayed if keywords does not exist in localStorage
-    let storedKeywords = getArrayFromLocal("keywords_" + network) != null ? getArrayFromLocal("keywords_" + network) : ['example keyword']; 
+    let storedKeywords = getArrayFromLocal("keywords_" + network) != null ? getArrayFromLocal("keywords_" + network) : ['example keyword'];
 
     const [tags, setTags] = React.useState(storedKeywords);
 
-    function renderKeywordAssets(){
+    /* Basically does the same thing in Bookmarks.js to get the bookmarks from storage and render them */
 
+    // Keeps track of fetched new asset data 
+    let [assetResults, setAssetResults] = useState("");
+
+    function getKeywordAssetsDDOs() {
+        let assetDids = getArrayFromLocal("keywordAssets_" + network);
+
+        // Create an array of promises to evaluate
+        let promiseArray = [];
+        assetDids.forEach((did) => {
+            promiseArray.push(fetchDDO(did));
+        });
+
+        // Once all promises are done, update assetResults with the data
+        Promise.all(promiseArray).then((values) => {
+            console.log(values);
+            setAssetResults({ results: values });
+        });
     }
+
+    function renderKeywordAssets(){
+        // if asset results is empty, fetch it
+        if (assetResults == "") {
+            getKeywordAssetsDDOs();
+            console.log("rendering keyword assets");
+        }
+        return (
+            assetResults != "" ? <MarketAssetList results={assetResults}> </MarketAssetList> : null
+        );
+    }
+
+    /* -------- */
 
     return (
         <Panel>
@@ -48,16 +78,8 @@ let Alerts = props => {
                     // save current date to localStorage
                     storeToLocal("keywordDate_" + network, new Date().toISOString());
                 }}
-            /> 
-            <Button
-                onClick={() => {
-                    console.log("refreshed");
-                }}
-            >
-                <div className="mx-2">
-                    refresh
-                </div>
-            </Button> 
+            />
+            {renderKeywordAssets()}
         </Panel>
     );
 }
