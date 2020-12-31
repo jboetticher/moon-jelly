04935e9a9ce62600670c4f3cffd9806d862df4bc
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2020-12-30 16:53:13 -0800

    Experimented with ocean pool

--- moon-jelly-ext/src/modules/balancer_pool_module/BalancerPool.js
+++ moon-jelly-ext/src/modules/balancer_pool_module/BalancerPool.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Ocean } from '@oceanprotocol/lib';
 import { OceanPool } from '@oceanprotocol/lib/dist/node/balancer/OceanPool';
+import { useOcean } from '@oceanprotocol/react';
 
 let BalancerPool = props => {
 
-    let pool = new OceanPool();
+    let ocean = useOcean();
+    console.log(ocean);
+    let pool = new OceanPool(ocean.web3, null, null, null, null, '0xB602b33f5b207984B2ac9cCb378ddEe78B4D29da');
     console.log(pool);
 
     return(
