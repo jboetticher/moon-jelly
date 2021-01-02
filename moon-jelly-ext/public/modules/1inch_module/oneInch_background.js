9b26ea4921c7141a9faa9506b670791e9e575c27
pisslake <pisslake@users.noreply.github.com>
2021-01-01 19:15:34 -0800

    getAllQuotes() tested, fixed, and works

--- moon-jelly-ext/public/modules/1inch_module/oneInch_background.js
+++ moon-jelly-ext/public/modules/1inch_module/oneInch_background.js
@@ -1,14 +1,9 @@
-// CONTROLS THE NETWORK THE ALERT SYSTEM IS ON
-// 1INCH IS ALWAYS ON MAINNET
-// CONTROLS JUST THE OCEAN FETCHING
-//const network = 'mainnet';
-const network = 'rinkeby';
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.name === "storageUpdate")
 
-        console.log("storage update received in 1inch script");
+    console.log("storage update received in 1inch script");
     //startAlarm(); temp disabled for testing
+    //getAllQuotes().then(data => console.log(data));
 });
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
@@ -71,23 +66,25 @@ function checkTriggers() {
 }
 
 // Returns a Promises.all() promise containing all the DDOs of oneInchAlertList
-function getAllDDOs(){
+function getAllDDOs() {
 
 }
 
 // Returns a Promises.all() promise containing 1inch quotes for every type of tokens in oneInchAlertList
 // Should prevent/reduce over-fetching of a token
-function getAllQuotes(){
-    let alertList = window.localStorage.getItem("oneInchAlertList");
+function getAllQuotes() {
+    // Get the stored array by parsing
+    let alertList = JSON.parse(window.localStorage.getItem("oneInchAlertList"));
 
     // Store token addresses as a set to prevent duplicates
     let tokenSet = new Set();
 
     // Loop through the assets
     alertList.forEach((asset, i) => {
+        console.log("this is the asset", asset);
 
         // Get the array of alerts in the asset
-        let currEntries = JSON.parse(asset['entries']);
+        let currEntries = asset['entries'];
 
         // Loop through each alert entry
         currEntries.forEach((entry, j) => {
@@ -101,14 +98,14 @@ function getAllQuotes(){
     let quotePromises = [];
 
     // Loop through each token address and create a promise
-    tokenSet.forEach((tokenAddress)=>{
+    tokenSet.forEach((tokenAddress) => {
 
         // Add fetch promise into array
         quotePromises.push(fetchOneInchQuote(tokenAddress));
     });
 
     // Return one big promise
-    return Promise.all(quotePromises); 
+    return Promise.all(quotePromises);
 }
 
 
