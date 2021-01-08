109eb4d832fe62f832e9108e82aa9dd0c459fcb0
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-01-07 18:27:26 -0800

    Fixed not connected issue with ocean pool module

--- moon-jelly-ext/src/modules/balancer_pool_module/BalancerPool.js
+++ moon-jelly-ext/src/modules/balancer_pool_module/BalancerPool.js
@@ -1,45 +1,50 @@
-import React, { useState } from 'react';
-import { OceanPool } from '@oceanprotocol/lib/dist/node/balancer/OceanPool';
-import { useOcean } from '@oceanprotocol/react';
+import { useWalletReady } from '../../functionality/CustomOceanHooks.js';
 import { useBookmarks } from '../../functionality/BookmarkHooks.js';
+import { useOcean } from '@oceanprotocol/react';
+import React, { useState } from 'react';
+import ConnectPanel from '../../components/ConnectPanel.js';
 import AssetList from '../../components/AssetList.js';
 import PoolAsset from './PoolAsset.js';
 import './Pool.css';
 
 let BalancerPool = props => {
 
+    const { walletConnected: isWalletConnected } = useWalletReady();
     const { getBookmarkDDOs } = useBookmarks();
     let [ddos, setDDOs] = useState();
-    if (ddos == null) {
+    if (ddos == null && isWalletConnected) {
         // sorts out all of the bookmarked pools and then sets them to state
-        getBookmarkDDOs().then(res => {
-            let sortedDDOList = [];
-            res.map((x, i) => {
-                if (x.price.type == "pool") sortedDDOList.push(x);
+        try {
+            getBookmarkDDOs().then(res => {
+                let sortedDDOList = [];
+                res.map((x, i) => {
+                    if (x.price.type == "pool") sortedDDOList.push(x);
+                });
+                setDDOs(sortedDDOList);
             });
-            setDDOs(sortedDDOList);
-        });
-    }
-    console.log(ddos);
-
-    let ocean = useOcean();
-    console.log(ocean);
-    //let pool = new OceanPool(ocean.web3, null, null, null, null, '0xB602b33f5b207984B2ac9cCb378ddEe78B4D29da');
-    //console.log(pool);
-
-    function examinePool(data) {
-        console.log(data);
+        }
+        catch(res) {
+            setDDOs([]);
+            console.log("there was an error with getting bookmarked ddos", res);
+        }
+        
+        console.log(ddos);
     }
 
-    return (
+    console.log("yeah it updated", isWalletConnected);
+
+    let panel = isWalletConnected ?
         <>
             <div>Ocean Pools</div>
             <p className="pool-info mb-2">
                 All of your bookmarked pools will appear here. Easily view details on each pool.
-            </p>
-            <AssetList results={ddos} assetEntry={PoolAsset} examinePool={examinePool} />
+        </p>
+            <AssetList results={ddos} assetEntry={PoolAsset} />
         </>
-    );
+        :
+        <ConnectPanel />;
+
+    return (panel);
 }
 
 export default BalancerPool;
